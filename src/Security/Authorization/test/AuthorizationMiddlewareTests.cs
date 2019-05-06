707374056029a70d48d8d3eb209df22720c9f5bd
Hao Kung <HaoK@users.noreply.github.com>
2019-05-06 15:35:45 -0700

    Move policy types back

--- src/Security/Authorization/test/AuthorizationMiddlewareTests.cs
+++ src/Security/Authorization/test/AuthorizationMiddlewareTests.cs
@@ -370,6 +370,7 @@ namespace Microsoft.AspNetCore.Authorization.Test
             serviceCollection.AddOptions();
             serviceCollection.AddLogging();
             serviceCollection.AddAuthorization();
+            serviceCollection.AddAuthorizationPolicyEvaluator();
             registerServices?.Invoke(serviceCollection);
 
             var serviceProvider = serviceCollection.BuildServiceProvider();
