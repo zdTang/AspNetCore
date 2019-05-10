2ac6887b3c2be03747fad7d8f775ea35a8f0f80b
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-05-10 11:20:29 -0700

    comment update

--- src/Http/Http.Abstractions/src/HttpRequest.cs
+++ src/Http/Http.Abstractions/src/HttpRequest.cs
@@ -104,7 +104,7 @@ namespace Microsoft.AspNetCore.Http
         public abstract Stream Body { get; set; }
 
         /// <summary>
-        /// Gets or sets the request body pipe <see cref="PipeReader"/>.
+        /// Gets the request body pipe <see cref="PipeReader"/>.
         /// </summary>
         public abstract PipeReader BodyReader { get; }
 
